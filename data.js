adb76239eaf6bd1dc367810012789fcdfd008255
E-RKP <edobackup111@gmail.com>
2025-05-19 16:51:23 +0200

    first version

---
+++ data.js
@@ -0,0 +1,78 @@
+
+
+function sommaArray (arr){
+
+  let somma = 0
+  for (i=0; i<arr.length;i++){
+    somma = somma+arr[i]
+  }
+  return somma;
+}
+
+console.log(sommaArray([0,1,2,3,4,66,89]))
+
+
+function sommaArray (arr){
+
+  let somma = 0
+  for (i=0; i<arr.length;i++){
+    somma = somma+arr[i]
+  }
+  return somma;
+}
+
+console.log(sommaArray([0,1,2,3,4,66,89]))
+
+function sommaArray (arr){
+
+  let somma = 0
+  for (i=0; i<arr.length;i++){
+    somma = somma+arr[i]
+  }
+  return somma;
+}
+
+console.log(sommaArray([0,1,2,3,4,66,89]))
+
+function sommaArray (arr){
+
+  let somma = 0
+  for (i=0; i<arr.length;i++){
+    somma = somma+arr[i]
+  }
+  return somma;
+}
+
+console.log(sommaArray([0,1,2,3,4,66,89]))
+
+function tabell (n){
+
+  for (i=0; i<n;i++){
+    console.log(`${i}X${i}=${i*i}`)
+  }
+}
+
+tabell(10);
+
+const button = document.getElementById('button')
+
+button.onclick = function saluto (){
+  alert('CIAO!')
+}
+
+function vocalcounter (string){
+  
+  let counter = 0
+  for (i=0; i<string.length;i++){
+      if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u'){
+    counter++;
+  }
+  }
+ 
+
+  return counter;
+}
+
+console.log(vocalcounter('ciao'))
+
+
